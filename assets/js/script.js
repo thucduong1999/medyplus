e7090178ec207250f54022604c5f243f1d0da27a
Nguyễn Thực Đương <72158025+thucduong1999@users.noreply.github.com>
2021-11-03 18:12:42 +0700

    AHT - update navbar + responsive header and navbar - duongnt

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Click để hiện thông tin web
+    $('.web-info-btn').click(function (e) { 
+        e.preventDefault();
+        $(this).find('.info-content').toggle('show');
+        $(this).siblings().toggle('hidden');
+    });
+    // Click để hiện navigation
+    $('.nav-icon').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').addClass('active');
+        $('.cover').addClass('active');
+    });
+    $('.cover').click(function (e) { 
+        e.preventDefault();
+        $('.navbar').removeClass('active');
+        $('.cover').removeClass('active');
+    });
+});
